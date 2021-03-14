9b9ac24ed6523fe4341daa6841ec8df7201dd807
William Barth <barthwp@dukes.jmu.edu>
2021-03-14 11:49:35 -0400

    Revert "Changed Note table in addServ(needs to be adjusted / new table needs to be added to SQL)"
    This reverts commit cab789ac7ea6ddbb54e0bf0cc39c8d3c355dd7ec.

--- CapstoneProject/MovingForm.aspx.designer.cs
+++ CapstoneProject/MovingForm.aspx.designer.cs
@@ -114,15 +114,6 @@ namespace CapstoneProject
         protected global::System.Web.UI.WebControls.Button btnCommit;
 
         /// <summary>
-        /// btnNext control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnNext;
-
-        /// <summary>
         /// btnBack control.
         /// </summary>
         /// <remarks>
