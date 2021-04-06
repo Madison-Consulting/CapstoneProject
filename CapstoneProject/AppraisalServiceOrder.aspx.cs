e3a107b1ae78b7045edede9ed3f94d95c93e32cd
Caitlyn Meyer <meyer5cm@dukes.jmu.edu>
2021-04-05 21:22:53 -0400

    allow for file upload on ASO

--- CapstoneProject/AppraisalServiceOrder.aspx.cs
+++ CapstoneProject/AppraisalServiceOrder.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace CapstoneProject
 {
@@ -27,6 +31,38 @@ namespace CapstoneProject
         protected void btnAuctionAssess_Click(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            string filename = Path.GetFileName(upldPhoto.PostedFile.FileName);
+            string contentType = upldPhoto.PostedFile.ContentType;
+            using (Stream fs = upldPhoto.PostedFile.InputStream)
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    byte[] bytes = br.ReadBytes((Int32)fs.Length);
+                    string constr = ConfigurationManager.ConnectionStrings["Lab3"].ConnectionString;
+                    using (SqlConnection con = new SqlConnection(constr))
+                    {
+                        string query = "INSERT INTO ApprasialServiceOrder VALUES @Name WHERE CustomerID = @CustomerID";
+                        using (SqlCommand cmd = new SqlCommand(query))
+                        {
+                            cmd.Connection = con;
+                            cmd.Parameters.AddWithValue("@Name", filename);
+                            cmd.Parameters.AddWithValue("@CustomerID", txtCustomerID.Text);
+
+                            //cmd.Parameters.AddWithValue("@ContentType", contentType);
+                            //cmd.Parameters.AddWithValue("@Data", bytes);
+                            con.Open();
+                            cmd.ExecuteNonQuery();
+                            con.Close();
+                        }
+                    }
+                }
+            }
+
+            lblMessage.Text = "File uploaded successfully.";
         }
     }
 }
